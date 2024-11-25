318d780e3c85da45c6b1fe5bf78e213081a40aae
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2024-11-25 13:04:54 +0100

    Refactor API calls to use environment variable for Firebase Realtime Database URL

--- app/create/page.js
+++ app/create/page.js
@@ -2,22 +2,21 @@ import { redirect } from "next/navigation";
 import FormPost from "../components/FormPost";
 
 export default function CreatePage() {
+  const url = `${process.env.NEXT_PUBLIC_FB_DB_URL}/posts.json`; // Get Firebase Realtime Database URL
+
   async function createPost(formData) {
     "use server";
     const caption = formData.get("caption");
     const image = formData.get("image");
 
-    const response = await fetch(
-      "https://fb-rest-race-default-rtdb.firebaseio.com/posts.json",
-      {
-        method: "POST",
-        body: JSON.stringify({
-          caption,
-          image,
-          uid: "OPPe5jue2Ghxx3mtnxevB5FwCYe2"
-        })
-      }
-    );
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        caption,
+        image,
+        uid: "OPPe5jue2Ghxx3mtnxevB5FwCYe2"
+      })
+    });
 
     if (response.ok) {
       redirect("/posts");
