ad70d4faaa612d0e3ca33072631e4e8881a625d8
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2024-11-28 10:40:43 +0100

    Improve text clarity on home page and enhance link description

--- app/page.js
+++ app/page.js
@@ -5,8 +5,8 @@ export default function Home() {
     <main className="page">
       <section className="container">
         <h1>Post App</h1>
-        <p>This is my post app</p>
-        <Link href="/posts">Go to Posts.</Link>
+        <p>This is my post app.</p>
+        <Link href="/posts">Go to Posts to see all posts.</Link>
       </section>
     </main>
   );
