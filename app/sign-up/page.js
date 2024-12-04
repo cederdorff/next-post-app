9129aa8c4c85e505438c64bbf139ce23588ac0b2
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2024-12-04 21:32:43 +0100

    Update page.js

--- app/sign-up/page.js
+++ app/sign-up/page.js
@@ -12,23 +12,23 @@ export default async function SignIn() {
   }
 
   async function handleSignUpWithEmailAndPassword(currentState, formData) {
-    "use server";
-
-    console.log("formData", formData);
+    "use server"; // this code will run on the server only
 
     const name = formData.get("name");
     const email = formData.get("email");
     const password = formData.get("password");
 
-    // check if the user already exists
+    // check if the user already exists in database
     const user = await getUserByMail(email);
     if (user) {
       return { message: "User with this email already exists. ", name, email };
     }
 
+    // hash the password
     const salt = await bcrypt.genSalt(10); // generate a salt
     const hashedPassword = await bcrypt.hash(password, salt); // hash the password
 
+    // create the user
     await createUser({
       name,
       mail: email,
