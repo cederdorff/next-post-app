20324ebd1624e560e0fbbd3e93a6474bc6c58f72
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2024-12-06 10:38:01 +0100

    Add SignIn page with session check and redirect to profile

---
+++ app/sign-up/page.js
@@ -0,0 +1,20 @@
+import { redirect } from "next/navigation";
+import { getSession } from "../auth";
+import FormSignIn from "../components/FormSignIn";
+
+export default async function SignIn() {
+  const session = await getSession();
+  // if the user is already signed in, redirect them to the profile page
+  if (session) {
+    redirect("/profile");
+  }
+
+  return (
+    <main id="sign-in-page" className="page">
+      <section className="container">
+        <h1>Sign In</h1>
+        <FormSignIn />
+      </section>
+    </main>
+  );
+}
